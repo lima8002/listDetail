057a5dab2dcb81d0db613f4a98212f37db51c7f6
lima8002 <lima8002@gmail.com>
2020-08-24 19:18:18 +1000

    Added Data to HomeScreen

--- App.js
+++ App.js
@@ -10,11 +10,24 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator()
 
+const listData = [
+  {  "amount": 50,  "category": "food",  "id": "1598241633",  "note": "buying lunch"},
+  {  "amount": 20,  "category": "transport",  "id": "1598241768",  "note": "catching train"},
+  {  "amount": 80,  "category": "groceries",  "id": "1598241782",  "note": "shopping at Coles"},
+  {  "amount": 13,  "category": "food",  "id": "1598241795",  "note": "snack time"},
+  {  "amount": 35,  "category": "entertainment",  "id": "1598241806",  "note": "buying Untitled Goose"},
+  {  "amount": 350,  "category": "rent",  "id": "1598241817",  "note": "weeks rent"},
+  {  "amount": 60,  "category": "transport",  "id": "1598241827",  "note": "topping up Opal card"},
+  {  "amount": 30,  "category": "food",  "id": "1598241841",  "note": "buying dinner"}
+  ]
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home">
+          { (props) => <HomeScreen {...props} text='Hi there' data={listData}/>}
+        </Stack.Screen> 
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,51 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 export const HomeScreen = (props) => {
+    
     const navigation = useNavigation()
+
+    const ListItem = ( props ) => (
+        <View style={styles.list}>
+          <Text>{props.category}</Text>
+          <Text>$ {props.amount}</Text>
+        </View>
+      )
+
+    const renderList = ({ item }) => (
+        <ListItem 
+            id={item.id} 
+            amount={item.amount}
+            category={item.category}
+         />
+    )
+
+    const showDetails = ( item ) => {
+
+    }
 
     return (
         <View>
-            <Text>Home Screen</Text>
-            <Button title='Go to Detail' onPress={() => {navigation.navigate('Detail')}} />
+            <Text>{props.text}</Text>
+            {/*<Button title='Go to Detail' onPress={() => {navigation.navigate('Detail')}} />*/}
+            <FlatList
+                data={props.data}
+                renderItem={renderList}
+                keyExtractor={item => item.id}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    list:{
+        paddingHorizontal: 15,
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: 'lightgray',
+        flexDirection:'row',
+        justifyContent: 'space-between',
 
+    },
 })
